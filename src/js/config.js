be372bd13aa3266affa0f08473807ac863018c25
xmagnetisx <muhammad.trad@yahoo.com>
2019-09-01 20:44:47 -0700

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//export const key = 'b6a1725e1a1e5a4b857591ba98c0b005';
+//export const key = '8fe8514a284132a20603e66a1a19de65';
+//export const key = 'd8c4cb6986674f0cfd07bb4c898ef273';
+export const key =   'a51f3e7af9ddcddfa4ffee4be4ef2e30'; 
+//export const key = '7fc73d9fb16ffcdc56bdf61aed3ffc76' ;
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
